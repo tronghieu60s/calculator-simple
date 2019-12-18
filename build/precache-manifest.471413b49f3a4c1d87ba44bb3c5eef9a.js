edfa79650517694127ec15aa55ed28e5d79486ff
Trần Trọng Hiếu <tronghieu60s@gmail.com>
2019-12-18 12:51:13 +0700

    update readme, push build

---
+++ build/precache-manifest.471413b49f3a4c1d87ba44bb3c5eef9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e9087ebe242d793a96acf0d03a8d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbd25de4660758b1c3de",
+    "url": "/static/css/main.f076d2a5.chunk.css"
+  },
+  {
+    "revision": "513bdda1ce897f2c4600",
+    "url": "/static/js/2.5d745444.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5d745444.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbd25de4660758b1c3de",
+    "url": "/static/js/main.d6ebb42e.chunk.js"
+  },
+  {
+    "revision": "c64e01d26e8afdf80c8a",
+    "url": "/static/js/runtime-main.740e68dc.js"
+  }
+]);
